445390530996abd643b86ae88ec7a40479376f89
Walter Poch <walter.poch@gmail.com>
2015-03-16 20:12:14 -0300

    Before change the isEnabled setting.

--- mule-agent-db-event-tracker/src/main/java/com/mulesoft/agent/eventtracking/DatabaseEventTrackingAgent.java
+++ mule-agent-db-event-tracker/src/main/java/com/mulesoft/agent/eventtracking/DatabaseEventTrackingAgent.java
@@ -1,26 +1,21 @@
 package com.mulesoft.agent.eventtracking;
 
 import com.mchange.v2.c3p0.DataSources;
-import com.mulesoft.agent.AgentEnableOperationException;
 import com.mulesoft.agent.buffer.BufferedHandler;
 import com.mulesoft.agent.configuration.Configurable;
 import com.mulesoft.agent.domain.tracking.AgentTrackingNotification;
-import com.sun.deploy.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Named;
 import javax.inject.Singleton;
 import javax.sql.DataSource;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.Date;
 
 @Named("database.agent.eventtracking")
 @Singleton
@@ -48,6 +43,7 @@ public class DatabaseEventTrackingAgent extends BufferedHandler<AgentTrackingNot
     @Override
     public void postConfigurable(){
         super.postConfigurable();
+        LOGGER.trace("Starting the DatabaseEventTrackingAgent");
 
         if(isNullOrWhiteSpace(driver)
                 ||isNullOrWhiteSpace(jdbcUrl)
@@ -67,6 +63,7 @@ public class DatabaseEventTrackingAgent extends BufferedHandler<AgentTrackingNot
             LOGGER.error(String.format("The DatabaseEventTrackingAgent (database.agent.eventtracking) couldn't load the database driver '%s'. " +
                     "Did you copy the JAR driver to the {MULE_HOME}/plugins/mule-agent-plugin/lib?", driver), e);
             setEnabled(false);
+            return;
         }
 
         try {
@@ -75,6 +72,7 @@ public class DatabaseEventTrackingAgent extends BufferedHandler<AgentTrackingNot
         } catch (SQLException e) {
             LOGGER.error(String.format("There was an error on the connection to the DataBase. Please review your agent configuration."), e);
             setEnabled(false);
+            return;
         }
 
         insertStatement = String.format("INSERT INTO %s (Action, Application, MuleMessage, MuleMessageId, NotificationType, Path, ResourceIdentifier, Timestamp, Source) " +
@@ -89,30 +87,48 @@ public class DatabaseEventTrackingAgent extends BufferedHandler<AgentTrackingNot
 
     @Override
     protected boolean flush(Collection<AgentTrackingNotification> messages) {
-        dump(messages);
+        if(isEnabled()){
+            LOGGER.trace(String.format("Flushing %s notifications.", messages.size()));
 
-        try(Connection connection = pooledDataSource.getConnection()){
-            try(PreparedStatement statement = connection.prepareStatement(insertStatement)){
-                for(AgentTrackingNotification notification : messages){
-                    statement.setString(1, notification.getAction());
-                    statement.setString(2, notification.getApplication());
-                    statement.setString(3, notification.getMuleMessage());
-                    statement.setString(4, notification.getMuleMessageId());
-                    statement.setString(5, notification.getNotificationType());
-                    statement.setString(6, notification.getPath());
-                    statement.setString(7, notification.getResourceIdentifier());
-                    statement.setLong(8, notification.getTimestamp());
-                    statement.setString(9, notification.getSource());
-                    statement.addBatch();
+            dump(messages);
+            Connection connection = null;
+            try{
+                connection = pooledDataSource.getConnection();
+                PreparedStatement statement = null;
+                try{
+                    statement = connection.prepareStatement(insertStatement);
+                    for(AgentTrackingNotification notification : messages){
+                        statement.setString(1, notification.getAction());
+                        statement.setString(2, notification.getApplication());
+                        statement.setString(3, notification.getMuleMessage());
+                        statement.setString(4, notification.getMuleMessageId());
+                        statement.setString(5, notification.getNotificationType());
+                        statement.setString(6, notification.getPath());
+                        statement.setString(7, notification.getResourceIdentifier());
+                        statement.setLong(8, notification.getTimestamp());
+                        statement.setString(9, notification.getSource());
+                        statement.addBatch();
+                    }
+
+                    statement.executeBatch();
+                } finally {
+                    if(statement != null) statement.close();
                 }
 
-                statement.executeBatch();
+                return true;
+            } catch (SQLException e) {
+                LOGGER.warn("Couldn't insert the tracking notifications.",e);
+                return false;
+            } finally {
+                if(connection != null) try {
+                    connection.close();
+                } catch (SQLException e) {
+                    LOGGER.error("Error closing the database.", e);
+                }
             }
-        } catch (SQLException e) {
-            LOGGER.warn("Couldn't insert the tracking notifications.",e);
         }
 
-        return true;
+        return false;
     }
 
     public static boolean isNullOrWhiteSpace(String a) {
@@ -120,25 +136,23 @@ public class DatabaseEventTrackingAgent extends BufferedHandler<AgentTrackingNot
     }
 
     private void dump(Collection<AgentTrackingNotification> notifications){
-        try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("C:\\dbagent-debug.txt", true)))) {
             for (AgentTrackingNotification notification : notifications){
-                out.println("Action: " + notification.getAction());
-                out.println("Application: " + notification.getApplication());
-                out.println("MuleMessage: " + notification.getMuleMessage());
-                out.println("MuleMessageId: " + notification.getMuleMessageId());
-                out.println("NotificationType: " + notification.getNotificationType());
-                out.println("Path: " + notification.getPath());
-                out.println("ResourceIdentifier: " + notification.getResourceIdentifier());
-                out.println("Timestamp: " + notification.getTimestamp());
-                out.println("Source: " + notification.getSource());
+                LOGGER.info("Action: " + notification.getAction());
+                LOGGER.info("Date: " + new Date());
+                LOGGER.info("Action: " + notification.getAction());
+                LOGGER.info("Application: " + notification.getApplication());
+                LOGGER.info("MuleMessage: " + notification.getMuleMessage());
+                LOGGER.info("MuleMessageId: " + notification.getMuleMessageId());
+                LOGGER.info("NotificationType: " + notification.getNotificationType());
+                LOGGER.info("Path: " + notification.getPath());
+                LOGGER.info("ResourceIdentifier: " + notification.getResourceIdentifier());
+                LOGGER.info("Timestamp: " + notification.getTimestamp());
+                LOGGER.info("Source: " + notification.getSource());
                 for(Annotation annotation : notification.getAnnotations()){
-                    out.println("\tAnnotationType: "+annotation.annotationType());
-                    out.println("\tClass: "+annotation.getClass());
+                    LOGGER.info("\tAnnotationType: " + annotation.annotationType());
+                    LOGGER.info("\tClass: " + annotation.getClass());
                 }
-                out.println("--------------------------------------------------------");
+                LOGGER.info("--------------------------------------------------------");
             }
-        }catch (IOException e) {
-            //
-        }
     }
 }
