2e27e970059a922c7188006175afeaf6a80c9590
Walter Poch <walter.poch@gmail.com>
2015-03-19 14:57:17 -0300

    Added the latest Splunk Java SDK Dependency.

--- mule-agent-eventtracking-internalhandlers/mule-agent-splunk-event-tracker/src/main/java/com/mulesoft/agent/eventtracking/EventTrackingSplunkInternalHandler.java
+++ mule-agent-eventtracking-internalhandlers/mule-agent-splunk-event-tracker/src/main/java/com/mulesoft/agent/eventtracking/EventTrackingSplunkInternalHandler.java
@@ -1,7 +1,11 @@
 package com.mulesoft.agent.eventtracking;
 
 import com.mulesoft.agent.buffer.BufferedHandler;
+import com.mulesoft.agent.configuration.Configurable;
+import com.mulesoft.agent.configuration.PostConfigure;
 import com.mulesoft.agent.domain.tracking.AgentTrackingNotification;
+import com.splunk.Service;
+import com.splunk.ServiceArgs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +18,37 @@ import java.util.Collection;
 public class EventTrackingSplunkInternalHandler extends BufferedHandler<AgentTrackingNotification> {
     private final static Logger LOGGER = LoggerFactory.getLogger(EventTrackingSplunkInternalHandler.class);
 
+    @Configurable
+    String user;
+
+    @Configurable
+    String pass;
+
+    @Configurable
+    String host;
+
+    @Configurable("8089")
+    int port;
+
+    @Configurable("https")
+    String scheme;
+
+    private Service service;
+
+    @PostConfigure
+    public void postConfigurable(){
+//        ServiceArgs loginArgs = new ServiceArgs();
+//        loginArgs.setUsername(this.user);
+//        loginArgs.setPassword(this.pass);
+//        loginArgs.setHost(this.host);
+//        loginArgs.setPort(this.port);
+//        loginArgs.setScheme(this.scheme);
+//
+//        service = Service.connect(loginArgs);
+        service = new Service("192.168.61.128", 8089);
+        service.login("admin", "test");
+    }
+
     @Override
     protected boolean canHandle(AgentTrackingNotification message) {
         return false;
@@ -21,6 +56,10 @@ public class EventTrackingSplunkInternalHandler extends BufferedHandler<AgentTra
 
     @Override
     protected boolean flush(Collection<AgentTrackingNotification> messages) {
+
+        for (AgentTrackingNotification notification : messages){
+            service.post(notification.toString());
+        }
         return false;
     }
 }
--- mule-agent-eventtracking-internalhandlers/mule-agent-splunk-event-tracker/src/test/java/com/mulesoft/agent/eventtracking/EventTrackingSplunkInternalHandlerTest.java
+++ mule-agent-eventtracking-internalhandlers/mule-agent-splunk-event-tracker/src/test/java/com/mulesoft/agent/eventtracking/EventTrackingSplunkInternalHandlerTest.java
@@ -1,12 +1,38 @@
 package com.mulesoft.agent.eventtracking;
 
+import com.mulesoft.agent.domain.tracking.AgentTrackingNotification;
 import junit.framework.Assert;
 import org.junit.Test;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventTrackingSplunkInternalHandlerTest {
 
     @Test
     public void test(){
-        Assert.assertTrue(true);
+        EventTrackingSplunkInternalHandler handler = new EventTrackingSplunkInternalHandler();
+        handler.user = "admin";
+        handler.pass = "test";
+        handler.host = "192.168.61.128";
+        handler.port = 8089;
+        handler.scheme = "https";
+        handler.postConfigurable();
+
+        handler.flush(createNotifications());
+
+        Assert.fail();
+    }
+
+    private List<AgentTrackingNotification> createNotifications(){
+        List<AgentTrackingNotification> list = new ArrayList<AgentTrackingNotification>();
+        for(int i = 0; i < 10; i++){
+            list.add(new AgentTrackingNotification.TrackingNotificationBuilder()
+                    .action("TEST " + i)
+                    .annotations(new ArrayList<Annotation>())
+                    .build());
+        }
+        return list;
     }
 }
