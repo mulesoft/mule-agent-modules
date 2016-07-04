4dad5af7d25399231dfeb82c784eadc7803e5e57
Sebastian G. Vinci <sgvinci@gmail.com>
2016-07-04 14:27:24 -0300

    AGENT-319 fix tests

--- mule-agent-monitoring-publishers/mule-agent-ingest-publisher/src/test/java/com/mulesoft/agent/monitoring/publisher/model/MemoryMetricSampleDecoratorTest.java
+++ mule-agent-monitoring-publishers/mule-agent-ingest-publisher/src/test/java/com/mulesoft/agent/monitoring/publisher/model/MemoryMetricSampleDecoratorTest.java
@@ -17,22 +17,22 @@ public class MemoryMetricSampleDecoratorTest
     public void complete()
     {
         MetricSample sample = new MemoryMetricSampleDecorator(new DefaultMetricSample(new Date(), testCases.complete()));
-        Assert.assertEquals(0.00000001862645149230957, sample.getMax());
-        Assert.assertEquals(0.000000001862645149230957, sample.getMin());
-        Assert.assertEquals(0.000000008226682742436727, sample.getAvg());
+        Assert.assertEquals(0.000019073486328125, sample.getMax());
+        Assert.assertEquals(0.0000019073486328125, sample.getMin());
+        Assert.assertEquals(0.000008424123128255209, sample.getAvg());
         Assert.assertEquals(3d, sample.getCount());
-        Assert.assertEquals(0.00000002468004822731018, sample.getSum());
+        Assert.assertEquals(0.000025272369384765625, sample.getSum());
     }
 
     @Test
     public void shouldNotThrowNPEWhenGivenACoupleOfNulls()
     {
         MetricSample sample = new MemoryMetricSampleDecorator(new DefaultMetricSample(new Date(), testCases.aCoupleOfNulls()));
-        Assert.assertEquals(0.00000001862645149230957, sample.getMax());
-        Assert.assertEquals(0.000000001862645149230957, sample.getMin());
-        Assert.assertEquals(0.000000008226682742436727, sample.getAvg());
+        Assert.assertEquals(0.000019073486328125, sample.getMax());
+        Assert.assertEquals(0.0000019073486328125, sample.getMin());
+        Assert.assertEquals(0.000008424123128255209, sample.getAvg());
         Assert.assertEquals(3d, sample.getCount());
-        Assert.assertEquals(0.00000002468004822731018, sample.getSum());
+        Assert.assertEquals(0.000025272369384765625, sample.getSum());
     }
 
     @Test
